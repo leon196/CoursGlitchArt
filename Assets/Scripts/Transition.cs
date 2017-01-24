f2e07d282a0d3e8555718cdd63e9547e46295538
Leon <lowpolygonum@gmail.com>
2017-01-24 21:32:12 +0100

    Unity3D project with basic transition script

---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class Transition : MonoBehaviour
+{
+
+	private Vector3 positionInitial;
+	private Quaternion rotationInitial;
+	private Camera cameraComponent;
+	private RaycastHit hitInfo;
+	private RenderTexture currentRender;
+	private RenderTexture renderTexture;
+
+	void Start ()
+	{
+		positionInitial = transform.position;
+		rotationInitial = transform.rotation;
+		cameraComponent = GetComponent<Camera>();
+		renderTexture = new RenderTexture(Screen.width, Screen.height, 16, RenderTextureFormat.ARGB32);
+	}
+	
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = cameraComponent.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hitInfo)) {
+				StartTransition(hitInfo.transform.position, hitInfo.transform.rotation);
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space) && transform.position != positionInitial) {
+			StartTransition(positionInitial, rotationInitial);
+		}
+	}
+
+	void OnRenderImage (RenderTexture src, RenderTexture dest)
+	{
+		Graphics.Blit(src, dest);
+		currentRender = dest;
+	}
+
+	public void StartTransition (Vector3 position, Quaternion rotation)
+	{
+		transform.position = position;
+		transform.rotation = rotation;
+
+		Graphics.Blit(currentRender, renderTexture);
+	}
+}
